2c8d8072c725e59ed9bce89f108a27c57ad87473
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-09-15 15:44:02 +0300

    Tasks from course DjangoBasic

--- djangoIntrocution/djangoIntrocution/settings.py
+++ djangoIntrocution/djangoIntrocution/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'djangoIntrocution.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
+        "NAME": "todo_db",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
