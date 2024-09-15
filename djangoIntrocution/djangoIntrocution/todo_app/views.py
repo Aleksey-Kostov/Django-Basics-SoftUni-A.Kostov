9691039f89c2fa23de7351e5fcbb3d948cae3690
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-09-15 16:57:12 +0300

    Tasks from course DjangoBasic

--- djangoIntrocution/djangoIntrocution/todo_app/views.py
+++ djangoIntrocution/djangoIntrocution/todo_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from djangoIntrocution.todo_app.models import Task
+
+
+def index(request):
+    title_filter = request.GET.get('title_filter', '')
+
+    tasks = Task.objects.filter(name__icontains=title_filter)
+
+    context = {
+        'title_filter': title_filter,
+        'tasks': tasks,
+    }
+
+    return render(request, 'tasks/index.html', context)  # MIME TYPE text/html
