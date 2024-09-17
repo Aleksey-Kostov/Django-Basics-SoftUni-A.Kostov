5eb26b5a96339c14461f4ff0709021dde4fc2da5
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-09-17 20:54:05 +0300

    Tasks from course DjangoBasic

--- djangoIntroduction/djangoIntroduction/urls.py
+++ djangoIntroduction/djangoIntroduction/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('djangoIntrocution.todo_app.urls'))
+    path('', include('djangoIntroduction.todo_app.urls'))
 ]
