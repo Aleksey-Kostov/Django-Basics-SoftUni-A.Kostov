4dd20afdf3d02cf8c761a3f94b2e90dff489ff15
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-09-17 20:44:58 +0300

    Tasks from course DjangoBasic

--- djangoIntroduction/djangoIntrocution/asgi.py
+++ djangoIntroduction/djangoIntrocution/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoIntrocution project.
+ASGI config for djangoIntroduction project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoIntrocution.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoIntroduction.settings')
 
 application = get_asgi_application()
