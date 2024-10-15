394dae4d7dcf073967254739b76a696a598889d6
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-15 16:26:48 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/settings.py
+++ petstagram/petstagram/petstagram/settings.py
@@ -30,6 +30,12 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MY_APP = ['photos',
+          'accounts',
+          'common',
+          'pets',
+          ]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
