ae5616b06b5077e635f88bba1531bb51c202aa6b
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-17 13:55:31 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/petstagram/pets/models.py
@@ -7,12 +7,20 @@ class Pet(models.Model):
 
     personal_photo = models.URLField()
 
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True,
+                                     null=True
+                                     )
 
-    slug = models.SlugField(unique=True, null=False, blank=True)
+    slug = models.SlugField(unique=True,
+                            null=True,
+                            blank=True,
+                            editable=False,
+                            )
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
 
-        super().save(*args, **kwargs)
+        return super().save(*args, **kwargs)
