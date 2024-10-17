9f4b599c70275955e26f6103fa78fe3f7ae780b4
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-17 12:15:05 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/petstagram/pets/urls.py
@@ -4,9 +4,9 @@ from petstagram.pets import views
 
 urlpatterns = [path('add/', views.pet_add_page, name='add-pet'),
                path('<str:username>/pet/<slug:pet_slug>', include([
-                   path('', views.pet_details_page, name='details-page'),
-                   path('edit', views.pet_edit_page, name='edit-page'),
-                   path('delete', views.pet_delete_page, name='delete-page')
+                   path('', views.pet_details_page, name='pet-details'),
+                   path('edit', views.pet_edit_page, name='edit-pet'),
+                   path('delete', views.pet_delete_page, name='delete-pet')
                ]))
 
                ]
