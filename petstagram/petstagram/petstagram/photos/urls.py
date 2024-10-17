ee5978a6373b3c0972fa6c075e989a79149ec856
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-17 19:04:31 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/photos/urls.py
+++ petstagram/petstagram/petstagram/photos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from petstagram.photos import views
 
-urlpatterns = [path('add/', views.photo_add_page, name='photo_add'),
+urlpatterns = [path('add/', views.photo_add_page, name='add-photo'),
                path('<int:pk>/', include([
                    path('', views.photo_details_page, name='photo-details'),
                    path('edit/', views.photo_edit_page, name='photo-edit'),
