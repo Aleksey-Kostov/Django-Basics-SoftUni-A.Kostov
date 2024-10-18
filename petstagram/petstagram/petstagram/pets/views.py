60184bc6e3bb0fb56ba3c566c8726162eec03450
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-18 21:40:46 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/petstagram/pets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 def pet_add_page(request):
     return render(request, 'pets/pet-add-page.html')
@@ -10,8 +12,17 @@ def pet_delete_page(request, username: str, pet_slug: str):
 
 
 def pet_details_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos
+    }
+    return render(request, 'pets/pet-details-page.html', context)
 
 
 def pet_edit_page(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-edit-page.html')
+
+
--- petstagram/petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/petstagram/photos/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from petstagram.common.models import Like
+from petstagram.photos.models import Photo
+
 
 def photo_add_page(request):
     return render(request, 'photos/photo-add-page.html')
 
 
 def photo_details_page(request, pk: int):
-    return render(request, 'photos/photo-details-page.html')
+    photo = Photo.objects.get(pk=pk)
+    likes = Like.objects.all()
+    comments = photo.comment_set.all()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments
+    }
+    return render(request, 'photos/photo-details-page.html', context)
 
 
 def photo_edit_page(request, pk: int):
