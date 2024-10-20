a78c6e6ec6d2fba06cf30fcacf83d45260e3c886
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-20 15:54:54 +0300

    Tasks from course DjangoBasic

--- petstagram/petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/petstagram/pets/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.pets.forms import PetAddForm
 from petstagram.pets.models import Pet
 
 
 def pet_add_page(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetAddForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('profile_details', pk=1)
+    context = {
+        'form': form
+               }
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_delete_page(request, username: str, pet_slug: str):
