1541ab2826af77b42df08ccb6e72c008098c8168
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-24 19:08:30 +0300

    Tasks from course DjangoBasic

--- exam_preparation/recipes_app/recipes_app/profile_recipes/models.py
+++ exam_preparation/recipes_app/recipes_app/profile_recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 
+class Profile(models.Model):
+    nickname = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+    )
+
+    chef = models.BooleanField(
+        default=False,
+    )
+
+    bio = models.TextField(
+        blank=True,
+    )
--- exam_preparation/recipes_app/recipes_app/recipe/models.py
+++ exam_preparation/recipes_app/recipes_app/recipe/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from recipes_app.profile_recipes.models import Profile
+from recipes_app.recipe.choices import CuisineChoices
+
+
+class Recipe(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    cuisine_type = models.CharField(
+        choices=CuisineChoices,
+        max_length=7,
+    )
+
+    ingredients = models.TextField(
+        help_text="Ingredients must be separated by a comma and space.",
+    )
+
+    instructions = models.TextField()
+
+    cooking_time = models.PositiveIntegerField(
+        help_text="Provide the cooking time in minutes.",
+    )
+
+    image_url = models.URLField(
+        blank=True,
+    )
+
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
