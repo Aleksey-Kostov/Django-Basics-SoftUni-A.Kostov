9e9570fa859ca32faf56be0f04599743919e9dd7
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-10-26 18:44:34 +0300

    Tasks from course DjangoBasic

--- regular_exam_27.10.2024_A.Kostov/regular_exam_project/regular_exam_project/urls.py
+++ regular_exam_27.10.2024_A.Kostov/regular_exam_project/regular_exam_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for exam_project project.
+URL configuration for regular_exam_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
